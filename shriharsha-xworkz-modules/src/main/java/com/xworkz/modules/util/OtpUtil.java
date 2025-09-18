c3ba6a5715c38b179da2bef58190f96363cff6d9
Shriharsha K M <shriharshakm10@gmail.com>
2025-09-18 14:40:54 +0530

    Add OTP utility interface and implementation for generating one-time passwords

---
+++ shriharsha-xworkz-modules/src/main/java/com/xworkz/modules/util/OtpUtil.java
@@ -0,0 +1,5 @@
+package com.xworkz.modules.util;
+
+public interface OtpUtil {
+    String genrateOtp();
+}
---
+++ shriharsha-xworkz-modules/src/main/java/com/xworkz/modules/util/OtpUtilImpl.java
@@ -0,0 +1,15 @@
+package com.xworkz.modules.util;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+@Component
+public class OtpUtilImpl implements OtpUtil {
+    @Override
+    public String genrateOtp() {
+        Random  random = new Random();
+        return String.valueOf(random.nextInt(99999));
+    }
+
+}
